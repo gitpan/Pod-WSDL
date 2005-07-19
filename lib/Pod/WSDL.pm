2a54a59301aec81d504a3b24113b086cfa8d03c9
Tarek Ahmed <bloerch@oelbsk.org>
2005-07-19 17:04:22 +0000

    Import of TAREKA/Pod-WSDL-0.02 from CPAN.
    gitpan-cpan-distribution: Pod-WSDL
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TAREKA/Pod-WSDL-0.02.tar.gz
    gitpan-cpan-author:       TAREKA
    gitpan-cpan-maturity:     released

--- lib/Pod/WSDL.pm
+++ lib/Pod/WSDL.pm
@@ -27,7 +27,7 @@ use Pod::WSDL::AUTOLOAD;
 # ------------------ > "CONSTANTS" ----------------------------------------- #
 # -------------------------------------------------------------------------- #
 
-our $VERSION = 0.01;
+our $VERSION = "0.02";
 our @ISA = qw/Pod::WSDL::AUTOLOAD/;
 
 our $WSDL_METHOD_REGEXP_BEG = qr/^=(?:begin)\s+wsdl\s*\n(.*?)^=(?:cut|end\s+wsdl).*?^\s*sub\s+(\w+)/ims;
